473cbbb990427e667f443be577cfb877d2e65836
wunan <wunan@qq.com>
2020-01-06 11:07:50 +0800

    第二次

--- UserDialog.py
+++ UserDialog.py
@@ -3,6 +3,7 @@ from tkinter import *
 class UserDialog:
     _Ip = ''
     _port = 0
+    _nickname = ''
 
     def __init__(self):
         pass
@@ -12,7 +13,7 @@ class UserDialog:
         def getUserIpAndPort():
             cls._Ip = e1.get()
             cls._port = int(e2.get())
-            print(cls._Ip,cls._port)
+            # print(cls._Ip,cls._port)
             ClientWindow.destroy()
 
         ClientWindow = Tk()
@@ -29,6 +30,26 @@ class UserDialog:
         button = Button(ClientWindow)
         button.config(text='ok',command = getUserIpAndPort)
         button.grid(row = 3, column= 0)
+
+        ClientWindow.mainloop()
+
+    @classmethod
+    def getUserNickName(cls):
+        def getUserNickNameInner():
+            cls._nickname = e1.get()
+
+            ClientWindow.destroy()
+
+        ClientWindow = Tk()
+        Label(ClientWindow, text='请输入昵称').grid(row=0)
+        Label(ClientWindow, text='昵称').grid(row=1)
+
+        e1 = Entry(ClientWindow)
+        e1.grid(row=1, column=1)
+
+        button = Button(ClientWindow)
+        button.config(text='ok', command=getUserNickNameInner)
+        button.grid(row=2, column=0)
 
         ClientWindow.mainloop()
 

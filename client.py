b94b671ca25d83edd23cb626f5c131d0b324670e
wunan <wunan@qq.com>
2020-01-08 14:04:57 +0800

    增加了画字和删除功能

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import time
 from threading import Thread
+
 from connection import Connection
 from whiteboard import WhiteBoard
-
+from UserDialog import UserDialog
 
 class Client(Thread,WhiteBoard):
-
-    Objects = {'line': 'L', 'oval': 'O', 'circle': 'C', 'rectangle': 'R', 'square': 'S', 'erase': 'E', 'drag': 'DR'}
+    Objects = {'line': 'L', 'oval': 'O', 'circle': 'C', 'rectangle': 'R', 'square': 'S', 'drag': 'DR'}
 
     def __init__(self):
         self.conn = Connection()
@@ -26,6 +26,17 @@ class Client(Thread,WhiteBoard):
         self.drawing_area.bind("<ButtonPress-1>", self.left_but_down)
         self.drawing_area.bind("<ButtonRelease-1>", self.left_but_up)
 
+    def send_del_msg(self,event):
+        canvas_item_tuple = self.drawing_area.find_overlapping(event.x + 2, event.y + 2, event.x - 2, event.y - 2)
+        print(canvas_item_tuple)
+        if len(canvas_item_tuple) > 0:
+            to_delete_id = max(canvas_item_tuple)
+            tags = self.drawing_area.gettags(to_delete_id)
+            msgid = tags[0]
+            msg = ('Z', msgid)
+            self.conn.send_message(msg)
+
+
     #(tpye，startx,starty,endx,endy,color)
     #('D',startx,starty,endx,endy,'red')
     def left_but_down(self,event=None):
@@ -35,13 +46,23 @@ class Client(Thread,WhiteBoard):
         self.last_time = time.time()
         self.line_x1, self.line_y1 = event.x,event.y
 
+        if self.isMouseDown == True and self.drawing_tool == 'eraser':
+            self.send_del_msg(event)
 
     def left_but_up(self,event=None):
         self.isMouseDown = False
         print(event.x,event.y)
         self.last_time = None
         self.line_x2, self.line_y2 = event.x, event.y
-        self.draw_one_obj()
+        if self.drawing_tool == 'text':
+            self.draw_text()
+        else:
+            self.draw_one_obj()
+
+    def draw_text(self):
+        text_to_draw = UserDialog._Text
+        msg = ('T', self.line_x1, self.line_y1, 'red',text_to_draw)
+        self.conn.send_message(msg)
 
     def draw_one_obj(self):
         tool = self.drawing_tool
@@ -60,13 +81,15 @@ class Client(Thread,WhiteBoard):
             if now - self.last_time < 0.02:
                 print('too fast')
                 return
-
             self.last_time = now
-
             msg = ('D',self.x_pos,self.y_pos,event.x,event.y,'red')
             self.conn.send_message(msg)
             self.x_pos = event.x
             self.y_pos = event.y
+        elif self.isMouseDown == True and self.drawing_tool == 'eraser':
+            self.send_del_msg(event)
+
+
 
 
     def run(self):
@@ -77,7 +100,8 @@ class Client(Thread,WhiteBoard):
             print(msg)
             if msg == 'xxx':
                 pass
-
+            # print('i am running')
+            # time.sleep(0.1)
 
 if __name__ == '__main__':
     client = Client()
--- whiteboard.py
+++ whiteboard.py
@@ -1,4 +1,8 @@
+import math
 from tkinter import *
+from tkinter import font
+
+from UserDialog import UserDialog
 
 class WhiteBoard:
     drawing_tool = "line"
@@ -7,23 +11,74 @@ class WhiteBoard:
     #           'd': 'black', 's': 'snow'}
     line_width = 2
 
-    def draw_line(self, msgLst):
+    def draw_pencil(self, msgLst):
         startX,startY,endX,endY = int(msgLst[1]),int(msgLst[2]),int(msgLst[3]),int(msgLst[4])
         color = msgLst[5]
-        self.drawing_area.create_line(startX,startY,endX,endY,fill=color,width=self.line_width)
+        msgid = msgLst[6]
+        self.drawing_area.create_line(startX,startY,endX,endY,fill=color,width=self.line_width,tags=(msgid,))
 
     def draw_Rectangle(self, msgLst):
         startX,startY,endX,endY = int(msgLst[1]),int(msgLst[2]),int(msgLst[3]),int(msgLst[4])
         color = msgLst[5]
-        self.drawing_area.create_rectangle(startX,startY,endX,endY,fill=color,width=0)
+        msgid = msgLst[6]
+        self.drawing_area.create_rectangle(startX,startY,endX,endY,fill=color,width=0,tags=(msgid,))
+
+    def draw_Line(self, msgLst):
+        startX,startY,endX,endY = int(msgLst[1]),int(msgLst[2]),int(msgLst[3]),int(msgLst[4])
+        color = msgLst[5]
+        msgid = msgLst[6]
+        self.drawing_area.create_line(startX,startY,endX,endY,fill=color,width=self.line_width,tags=(msgid,))
+
+    def draw_Oral(self, msgLst):
+        startX,startY,endX,endY = int(msgLst[1]),int(msgLst[2]),int(msgLst[3]),int(msgLst[4])
+        color = msgLst[5]
+        msgid = msgLst[6]
+        self.drawing_area.create_oval(startX,startY,endX,endY,fill=color,width=0,tags=(msgid,))
+
+    def draw_Circle(self, msgLst):
+        startX,startY,endX,endY = int(msgLst[1]),int(msgLst[2]),int(msgLst[3]),int(msgLst[4])
+        x_center = (startX+endX) / 2
+        y_center = (startY+endY) / 2
+        radius = math.sqrt((endX - startX)**2 + (endY - startY)**2) / 2
+        color = msgLst[5]
+        msgid = msgLst[6]
+        self.drawing_area.create_oval(x_center-radius,y_center-radius,x_center+radius,y_center+radius,fill=color,width=0,tags=(msgid,))
+
+    def draw_Square(self, msgLst):
+        startX,startY,endX,endY = int(msgLst[1]),int(msgLst[2]),int(msgLst[3]),int(msgLst[4])
+        color = msgLst[5]
+        edge_size = ((endX-startX) + (endY - startY)) / 2
+        msgid = msgLst[6]
+        self.drawing_area.create_rectangle(startX,startY,startX+edge_size,startY+edge_size,fill=color,width=0,tags=(msgid,))
+
+    def draw_Text(self, msgLst):
+        textLst = msgLst[4:-1]
+        text = ' '.join(textLst)
+        x,y = msgLst[1],msgLst[2]
+        color = msgLst[3]
+        msgid = msgLst[-1]
+        text_font = font.Font(family='Helvetica', size=50, weight='bold', slant='italic')
+        self.drawing_area.create_text(x, y, fill=color, font=text_font, text=text,tags=(msgid,))
 
     def draw_from_msg(self,msg):
         msgLst = msg.split()
         draw_type = msgLst[0]
         if draw_type == 'D':
-            self.draw_line(msgLst)
-        if draw_type == 'R':
+            self.draw_pencil(msgLst)
+        elif draw_type == 'R':
             self.draw_Rectangle(msgLst)
+        elif draw_type == 'L':
+            self.draw_Line(msgLst)
+        elif draw_type == 'O':
+            self.draw_Oral(msgLst)
+        elif draw_type == 'C':
+            self.draw_Circle(msgLst)
+        elif draw_type == 'S':
+            self.draw_Square(msgLst)
+        elif draw_type == 'T':
+            self.draw_Text(msgLst)
+        elif draw_type == 'Z':
+            self.delete_obj(msgLst)
         else:
             pass
 
@@ -33,6 +88,11 @@ class WhiteBoard:
         self._init_item_button()
         self._init_color_button()
         self.init_drawing_area()
+
+    def delete_obj(self,msgLst):
+        # self.drawing_area.delete(msgLst[1])
+        item = self.drawing_area.find_withtag(msgLst[1])
+        self.drawing_area.delete(item)
 
     def show_window(self):
         self.myWhiteBoard.mainloop()
@@ -52,6 +112,10 @@ class WhiteBoard:
         print(color)
         self.color = color
 
+    def get_text_from_user(self):
+        WhiteBoard.drawing_tool = 'text'
+        UserDialog.get_text_from_user()
+
     def _init_item_button(self):
         Button(self.myWhiteBoard, text='line', height=1, width=5, bg='dark goldenrod', font='Arial',
                command=lambda: self.set_drawing_tool('line')).place(x=70, y=0)
@@ -59,8 +123,8 @@ class WhiteBoard:
                command=lambda: self.set_drawing_tool('rectangle')).place(x=140, y=0)
         Button(self.myWhiteBoard, text='oval', height=1, width=5, bg='NavajoWhite4', font='Arial',
                command=lambda: self.set_drawing_tool('oval')).place(x=210, y=0)
-        # Button(self.myWhiteBoard, text='text', height=1, width=5, bg='SteelBlue4', font='Arial',
-        #        command=self.get_text_from_user).place(x=280, y=0)
+        Button(self.myWhiteBoard, text='text', height=1, width=5, bg='SteelBlue4', font='Arial',
+               command=self.get_text_from_user).place(x=280, y=0)
         Button(self.myWhiteBoard, text='pencil', height=1, width=5, bg='DeepSkyBlue2', font='Arial',
                command=lambda: self.set_drawing_tool('pencil')).place(x=350, y=0)
         Button(self.myWhiteBoard, text='circle', height=1, width=5, bg='Turquoise2', font='Arial',
